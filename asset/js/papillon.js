8885494d96ce9a151784f2ff84221a4e707f5c30
samszo <samszon@gmail.com>
2021-12-29 06:35:16 +0100

    image

---
+++ asset/js/papillon.js
@@ -0,0 +1,136 @@
+let rdmEasingNames = d3.randomInt(0, 36), rdmX, rdmY
+    , nbNuee = urlParams.has('nbNuee') ?  urlParams.get('nbNuee') : 0
+    , cumulPapi = false
+    , nbPapi = urlParams.has('nbPapi') ?  urlParams.get('nbPapi') : 1
+    , papis = [], fctPapiAjoutEnd;
+
+function clouPapi(e, d){
+    let p = d3.select(e.currentTarget);
+    if(p.attr('class')!='clouPapi'){
+        arrAnim['#'+p.attr('id')].pause();        
+        p.attr('class','clouPapi');
+        //pour le dessin du pin merci à http://www.onlinewebfonts.com/icon
+        p.append('g')
+            .attr('transform','translate(300,300) scale(0.01)')//rotate(-45)
+            .append("path")
+                .attr("d","M6102.5,5015.5c-93.6-256.7-179.9-678.9-206.3-1012.4l-21.6-266.3l-1377-1374.6L3122.9,990.1l-167.9,24c-357.4,52.8-1029.2-4.8-1508.9-127.2l-115.2-28.8l930.8-930.8l933.2-933.2L1808.3-2392.5L421.7-3779.1l-163.1-508.6c-91.2-280.7-163.1-508.6-158.3-508.6c2.4,0,232.7,76.8,506.2,170.3l501.4,167.9L2492-3073.8l1386.6,1384.2l933.2-933.2L5745-3556l38.4,122.3c19.2,69.6,60,261.5,88.8,429.4c43.2,266.3,50.4,371.8,40.8,779.7l-12,470.2L7270.8-386.9L8640.5,980.5l215.9,16.8c119.9,9.6,302.3,31.2,407.8,50.4c227.9,43.2,635.7,153.5,635.7,175.1c0,7.2-851.6,873.2-1892.8,1921.6C6711.8,4449.4,6109.6,5039.5,6102.5,5015.5z")
+                .attr("fill","white");
+        //ajoute le drag & drop
+        p.call(
+                d3.drag()
+                    .on("start", dragstarted)
+                    .on("drag", dragged)
+                    .on("end", dragended)
+            );
+
+        /*
+        p.append('rect').attr('x',0).attr('y',0)
+            .attr("width",'100px').attr("height",'100px')
+            .attr('fill','white');
+        */
+
+        function dragstarted(e) {
+            e.sourceEvent.stopPropagation();
+        }
+        
+        function dragged(e) {
+            p.attr('x', e.x).attr('y', e.y);
+        }
+        
+        function dragended(e) {
+            console.log('dragended');
+        }              
+
+
+    } 
+}
+
+function ajoutNueePapillon(){
+    let svg = d3.select("#mesos");
+    let contPapi = d3.select("#nueePapillon");
+    if(!cumulPapi || nbPapi==0)contPapi.selectAll(".ChaoticumPapillonae").remove();
+    let posi = svg.node().getBBox();
+    rdmX=d3.randomUniform(posi.x, (posi.width-posi.x));
+    rdmY=d3.randomUniform(posi.y, (posi.height-posi.y));
+    for (var i = 0; i < nbPapi; i++) ajoutPapillon('ChaoticumPapillonae'+'_'+nbNuee+'_'+i, contPapi); 
+    nbNuee++;
+}
+
+function ajoutPapillon(id, svg, x=-1000, y=-1000, anime=true){
+    let url = "https://samszo.univ-paris8.fr/ChaoticumPapillonae/CreaPapiDynaAnim.php?anim=0&larg=64&haut=64&id="+id;
+    d3.xml(url)
+        .then(data => {
+            svg.node().append(data.documentElement);
+            //positionne le papillion
+            papis.push(
+                d3.select("#"+id)
+                .attr('x',x).attr('y',y)//le papillon s'affiche au premier déplacement
+                .attr('class','ChaoticumPapillonae')
+                .style('cursor','pointer')
+                .on('click',clouPapi)
+            );
+            //anime le papillon
+            if(anime)animePapi("#"+id);
+            if(fctPapiAjoutEnd)fctPapiAjoutEnd();   
+
+    });
+}
+
+function animePapi(trgt){
+    
+    arrAnim[trgt] = anime.timeline({
+        loop:true,
+    });
+    arrAnim[trgt]
+        .add({
+            targets: trgt,
+            x: getKeyFrame(rdmX),
+            y: getKeyFrame(rdmY),
+            });
+}
+
+function getKeyFrame(fct){
+    var keyframes = [];
+    for (var i = 0; i < 30; i++) keyframes.push({ 
+        value: fct(), duration: anime.random(2000, 4000), delay: anime.random(500, 2000), easing: easingNames[rdmEasingNames()] 
+    });
+    return keyframes;
+}
+let easingNames = [
+    'easeInQuad',
+    'easeInCubic',
+    'easeInQuart',
+    'easeInQuint',
+    'easeInSine',
+    'easeInExpo',
+    'easeInCirc',
+    'easeInBack',
+    'easeOutQuad',
+    'easeOutCubic',
+    'easeOutQuart',
+    'easeOutQuint',
+    'easeOutSine',
+    'easeOutExpo',
+    'easeOutCirc',
+    'easeOutBack',
+    'easeInBounce',
+    'easeInOutQuad',
+    'easeInOutCubic',
+    'easeInOutQuart',
+    'easeInOutQuint',
+    'easeInOutSine',
+    'easeInOutExpo',
+    'easeInOutCirc',
+    'easeInOutBack',
+    'easeInOutBounce',
+    'easeOutBounce',
+    'easeOutInQuad',
+    'easeOutInCubic',
+    'easeOutInQuart',
+    'easeOutInQuint',
+    'easeOutInSine',
+    'easeOutInExpo',
+    'easeOutInCirc',
+    'easeOutInBack',
+    'easeOutInBounce',
+];
---
+++ asset/js/textes.js
@@ -0,0 +1,134 @@
+let textes=[], nettoie=true, rdmTexte = true, regleVide = true
+, regle = urlParams.has('regle') ?  urlParams.get('regle') : 'MUTATIONS'
+, arrSourceTexte = [
+    {'label':'J-P Balpe - Centons de Corneille', 'gen':true,'fct':txtFromGen,'params':{'p':'oeu=31&cpt=157948&gen=727029&nb=6'}}
+    ,{'label':'J-P Balpe - Poèmes orientaux', 'gen':true,'fct':txtFromGen,'params':{'p':'oeu=37&cpt=171458&gen=727028&nb=6'}}
+    ,{'label':'J-P Balpe - Rengas', 'gen':true,'fct':txtFromGen,'params':{'p':'gen=727052'}}        
+    ,{'label':'Cantiquest - Chants Joyeux', 'gen':false,'fct':txtFromCsv,'params':{'url':'data/chants-joyeux.csv'}}
+    ,{'label':'Cantiquest - Chants de Victoire', 'gen':false,'fct':txtFromJson,'params':{'url':'data/chants-victoire.json'}}
+]
+, curSource = urlParams.has('source') ?  arrSourceTexte[urlParams.get('source')] : arrSourceTexte[d3.randomInt(0, arrSourceTexte.length)()];
+//charge les textes
+curSource.fct(curSource);
+
+function txtFromGen(d){
+    if(!d)d=curSource
+    let url = "https://www.balpe.name/generateur/services/api.php?"+d.params.p;
+    d3.text(url).then(function(data) {
+        //création des textes 
+        textes = parsePhrases(data);
+        nettoie=false;
+        rdmTexte=false;
+        regleVide=true;            
+        creaMeso();
+    });   
+}
+
+function parsePhrases(text) {
+    let phrases=[], arrT, txts = text.split(/\r\n|\n|\r/);
+    txts.forEach(t=>{
+        //vérifie si des phrases sont présentes
+        if(t.match( /[^\.!\?]+[\.!\?]+/g ))
+            //merci à https://stackoverflow.com/questions/11761563/javascript-regexp-for-splitting-text-into-sentences-and-keeping-the-delimiter
+            arrT = t.match( /[^\.!\?]+[\.!\?]+/g )
+                .map(function(t, i){return t});
+        else
+            arrT = [t];
+        phrases = phrases.concat(arrT);
+    });
+    return phrases;
+}        
+
+
+function txtFromCsv(e, d){
+
+    //Pour les textes merci à :
+    //https://cantiques.yapper.fr/CJ/index.html    
+    d3.csv('data/chants-joyeux.csv').then(function(data) {
+        //création des textes 
+        data.forEach(s=>{
+            let st = s.txt;
+            if(st!=""){
+                textes.push(st);                    
+            }
+        });
+        nettoie=true;
+        rdmTexte=true;
+        regleVide=false;            
+        creaMeso();
+    });
+}
+
+
+function txtFromJson(e, d){
+
+    //Pour les textes merci à :
+    //https://github.com/arnoldkouya/Chants-de-victoire-JSON    
+    d3.json('data/chants-victoire.json').then(function(data) {
+        //création des textes 
+        data.chants.forEach(c => {
+            c.content.forEach(s=>{                    
+                let arrS = s.substr(3).split(/\r\n|\n|\r/);
+                arrS.forEach(st=>{
+                    if(st!=""){
+                        textes.push(st)
+                    }                            
+                })
+            })
+        });
+        nettoie=true;
+        rdmTexte=true;
+        regleVide=false;            
+        creaMeso();
+    });   
+}
+
+
+function cleanTexte(s){
+    //nettoie le texte
+    s = s.replace('(Bis)','');
+    s = s.replace('(bis)','');
+    s = s.replace('!(bis)','!');
+    s = s.replace('»(bis)','');
+    s = s.replace('.(bis)','.');
+    s = s.replace('(ter)','');
+    s = s.replace('{','');
+    s = s.replace('}','');
+    s = s.replace('«','');
+    s = s.replace('»','');
+    s = s.replace(', etc.','');
+
+    //déchristianise le cantique
+    s = s.replace("Jésus Christ","l'Amour");
+    s = s.replace('Jésus',"l'Amour");
+    s = s.replace("Christ","l'Amour");
+    s = s.replace('Ta croix','ton arbre');
+    s = s.replace('de la croix',"d'un arbre");
+    s = s.replace('Sauveur',"ami");
+    s = s.replace('Le Seigneur',"le bonheur");
+    s = s.replace('Seigneur',"bonheur");
+    s = s.replace('Croix',"vie");
+    s = s.replace('croix',"vie");
+    s = s.replace('de Satan',"du triste");
+    s = s.replace('Satan',"le triste");
+    s = s.replace("l'agneau","l'enfant");
+    s = s.replace("l'Agneau","l'enfant");
+    s = s.replace("Agneau","enfant");
+    s = s.replace("David","lui");
+    s = s.replace("mon Dieu","mon pouvoir");
+    s = s.replace("Mon Dieu","mon pouvoir");
+    s = s.replace("de Dieu","du pouvoir");
+    s = s.replace("Dieu","le pouvoir");
+    s = s.replace("Divin","joyeux");
+    s = s.replace("divin","joyeux");
+    s = s.replace("Maître","ami");
+    s = s.replace("l'Évangile","le chant");
+    s = s.replace("l'évangile","le chant");
+    s = s.replace("sacrifié","aimé");
+    s = s.replace("“","");
+    s = s.replace("”","");
+    s = s.replace("Golgotha","ville");
+    
+            
+    return s        
+}
